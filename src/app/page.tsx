2a24b7d1595d27c694d3d73c79df2fe41240c8a4
Christian <chrisdev92@outlook.com>
2025-08-07 09:05:21 +0200

    improved structure and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import WalletButton from "@/components/wallet/WalletButton";
 import { WalletInfo } from "@/components/wallet/WalletInfo";
 export default function Home() {
   return (
-    <section className="container px-8 bg-amber-100 w-full h-auto">
-      <h1 className="text-2xl font-bold mb-6 text-center">Wallet Transaction Tracker</h1>
-      <div className="flex flex-col items-center">
-      <WalletButton />
-      <WalletInfo />
-      </div>
-    </section>
+      <section className="flex flex-row min-h-screen w-full">
+        <div className="w-full mt-10">
+          <h1 className="text-3xl font-bold mb-4 text-center">Wallet Transaction Tracker</h1>
+        </div>
+        <div className="flex flex-col mt-4 mr-8 min-w-[180px]">
+            <WalletButton />
+            <WalletInfo />
+        </div>
+      </section>
   );
 }
