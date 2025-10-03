96bd205859c4c0b67c32f41e62b1bf58337a31b9
Christian <chrisdev92@outlook.com>
2025-10-03 08:34:27 +0200

    refactore: enhance layout positioning in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import { WalletInfo } from "@/components/wallet/WalletInfo";
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-r from-blue-500 to-slate-900">
-      <header className="flex flex-row w-full">
+      <header className="relative flex flex-row w-full">
         <div className="w-full mt-10">
-          <h1 className="text-4xl font-bold mb-4 text-center text-white">
+          <h1 className="relative bottom-4 text-4xl font-bold mb-4 text-center text-white">
             Wallet Transaction Tracker
           </h1>
         </div>
-        <aside className="flex flex-col mt-4 mr-8 min-w-[180px]">
+        <aside className=" absolute top-4 right-4 flex flex-col min-w-[180px]">
           <WalletButton />
           <WalletInfo />
         </aside>
